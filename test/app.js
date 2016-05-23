a6095f9f743d08883a889ead4ad06fae1a39b4f4
半边 <itaofe@gmail.com>
2016-05-23 11:21:33 +0800

    test travis

--- test/app.js
+++ test/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
+
 describe('generator-rrrw:app', function () {
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
