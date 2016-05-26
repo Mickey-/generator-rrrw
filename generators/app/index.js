e5577895673ee645c886dd5637b08d4e953b7044
半边 <itaofe@gmail.com>
2016-05-26 15:44:58 +0800

    index.js readme surpport

--- generators/app/index.js
+++ generators/app/index.js
@@ -12,14 +12,44 @@ module.exports = yeoman.Base.extend({
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to 59store rrrw generator, it builds with' + chalk.red('react,redux,react-redux-router,webpack,babel,es6,autoprefixer,hmr,css-module')'
+      'Welcome to ' + chalk.cyan('59store rrrw generator') + ', it builds with ' + chalk.red('react,redux,react-redux-router,webpack,babel,es6,autoprefixer,hmr,css-module   ') + 'and so on'
     ));
 
     var prompts = [{
       type    : 'input',
-      name    : 'product',
+      name    : 'repo',
       message : '请输入项目仓库名称',
       default : this.env.cwd.split('/').pop() // Default 当前目录名
+    }, {
+      type    : 'input',
+      name    : 'product',
+      message : '请输入产品中文名称（如：店长PC Web后台系统）',
+      default : '59store大前端' 
+    }, {
+      type    : 'input',
+      name    : 'users',
+      message : '请输入面向的用户群（如：59store店长）',
+      default : '学生消费者'
+    }, {
+      type    : 'input',
+      name    : 'fe',
+      message : '请输入项目负责前端的花名，多人用逗号隔开',
+      default : '半边，换换，小巡'
+    }, {
+      type    : 'input',
+      name    : 'rd',
+      message : '请输入项目合作后端的花名，多人用逗号隔开',
+      default : '凌云，沉思'
+    }, {
+      type    : 'input',
+      name    : 'pd',
+      message : '请输入项目合作产品的花名，多人用逗号隔开',
+      default : '小易'
+    }, {
+      type    : 'input',
+      name    : 'ue',
+      message : '请输入项目合作设计师的花名，多人用逗号隔开',
+      default : '雷欧'
     }];
 
     return this.prompt(prompts).then(function (props) {
@@ -28,19 +58,21 @@ module.exports = yeoman.Base.extend({
   },
 
   writing: function () {
-    this.fs.copyTpl(
-      this.templatePath('package.json'),
-      this.destinationPath(),
-      this.props
-    );
+    var compilePath = this.templatePath('ejs_files/'),
+      files = fs.readdirSync(compilePath)
+
+    for(var i = 0; i < files.length; i++) {
+      this.fs.copyTpl(
+        this.templatePath(compilePath),
+        this.destinationPath(),
+        this.props
+      )
+    }
+
     this.directory(
-      this.templatePath('client/'),
+      this.templatePath('pure_files/'),
       this.destinationPath()
     );
-    this.fs.copy(
-      this.templatePath('webpack.config.js'),
-      this.destinationPath('webpack.config.js')
-    )
   },
 
   install: function () {
