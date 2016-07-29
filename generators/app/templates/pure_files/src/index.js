1066d07e87eb4c763845089632b00e2c43f8ef04
半边 <itaofe@gmail.com>
2016-07-29 15:53:02 +0800

    update template, distinguish pc and mobile

--- generators/app/templates/pure_files/src/index.js
+++ generators/app/templates/pure_files/src/index.js
@@ -1,14 +1,21 @@
-import 'normalize.css/normalize.css'
+<% if (mobileORpc == 'pc') { %>
+  import 'normalize.css/normalize.css'
+<% } else { %>
+  import 'm-base/index.less'
+  import FastClick from 'fastclick'
+  FastClick.attach(document.body)
+<% } %>
 import './base.less'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { Provider } from 'react-redux'
-
+import createStore from './store'
+// 按需增删改 start
 import RouteTest from './components/RouteTest'
 import App from './containers/App'
-import createStore from './store'
+// 按需增删改 end
 
 const store = createStore()
 const history = syncHistoryWithStore(hashHistory, store)
